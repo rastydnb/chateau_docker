e97054a463562efa004a2c7eb6d41410c3be4550
rastydnb <luisramos191@gmail.com>
2015-11-20 16:57:22 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+// RethinkDB settings
+exports.host = 'rethinkdb-master';    // RethinkDB host
+exports.port = 28015;          // RethinkDB driver port
+exports.authKey = '';          // Authentification key (leave an empty string if you did not set one)
+
+// Express settings
+exports.expressPort = 80;    // Port used by express
+exports.debug = true;          // Debug mode
+exports.network = '0.0.0.0'  // Network the node app will run on
